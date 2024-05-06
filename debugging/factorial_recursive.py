c2446e27878b7a1ed9f7bb4b4fa4290a78aa567a
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-06 15:43:44 -0400

    made executable

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,6 +2,16 @@
 import sys
 
 def factorial(n):
+    """
+    Function Description:
+    Calculates the factorial of a given number recursively.
+
+    Parameters:
+    - n: An integer representing the number whose factorial is to be calculated.
+
+    Returns:
+    An integer representing the factorial of the input number.
+    """
     if n == 0:
         return 1
     else:
