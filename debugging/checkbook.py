053d9e802233ed91f411302181677314dcdf037d
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-05-06 16:24:23 -0400

    added file

--- debugging/checkbook.py
+++ debugging/checkbook.py
@@ -3,17 +3,25 @@ class Checkbook:
         self.balance = 0.0
 
     def deposit(self, amount):
-        self.balance += amount
-        print("Deposited ${:.2f}".format(amount))
-        print("Current Balance: ${:.2f}".format(self.balance))
+        try:
+            amount = float(amount)
+            self.balance += amount
+            print("Deposited ${:.2f}".format(amount))
+            print("Current Balance: ${:.2f}".format(self.balance))
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
 
     def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient funds to complete the withdrawal.")
-        else:
-            self.balance -= amount
-            print("Withdrew ${:.2f}".format(amount))
-            print("Current Balance: ${:.2f}".format(self.balance))
+        try:
+            amount = float(amount)
+            if amount > self.balance:
+                print("Insufficient funds to complete the withdrawal.")
+            else:
+                self.balance -= amount
+                print("Withdrew ${:.2f}".format(amount))
+                print("Current Balance: ${:.2f}".format(self.balance))
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
 
     def get_balance(self):
         print("Current Balance: ${:.2f}".format(self.balance))
@@ -21,26 +29,19 @@ class Checkbook:
 def main():
     cb = Checkbook()
     while True:
-        action = input("What would you like to do? (deposit, withdraw, balance, exit): ").lower()
-        if action == 'exit':
+        action = input("What would you like to do? (deposit, withdraw, balance, exit): ")
+        if action.lower() == 'exit':
             break
-        elif action == 'deposit':
-            try:
-                amount = float(input("Enter the amount to deposit: $"))
-                cb.deposit(amount)
-            except ValueError:
-                print("Invalid input. Please enter a numeric value.")
-        elif action == 'withdraw':
-            try:
-                amount = float(input("Enter the amount to withdraw: $"))
-                cb.withdraw(amount)
-            except ValueError:
-                print("Invalid input. Please enter a numeric value.")
-        elif action == 'balance':
+        elif action.lower() == 'deposit':
+            amount = input("Enter the amount to deposit: $")
+            cb.deposit(amount)
+        elif action.lower() == 'withdraw':
+            amount = input("Enter the amount to withdraw: $")
+            cb.withdraw(amount)
+        elif action.lower() == 'balance':
             cb.get_balance()
         else:
             print("Invalid command. Please try again.")
-
 
 if __name__ == "__main__":
     main()
